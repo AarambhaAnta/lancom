010c12faedb662e1e3d08c87fdd1b86ead1c433a
AarambhaAnta <aarambha108@gmail.com>
2025-12-22 00:22:09 +0530

    feat: server start to listen

---
+++ server.go
@@ -0,0 +1,21 @@
+// server
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", "127.0.0.1:9000")
+	if err != nil {
+		fmt.Println("server: failed to start")
+		panic(err)
+	}
+	defer ln.Close()
+
+	fmt.Println("server: listening on 127.0.0.1:9000")
+
+	// Block forever to keep server running
+	select {}
+}
